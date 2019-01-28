b305aa1ded59c94e3a60ee6cd8fd88a60e8ad3fb
Alaa El Bouchti <alaa_23@hotmail.fr>
2019-01-28 18:01:43 +0100

    finxing bugs

--- inputs/images/dataset_images.py
+++ inputs/images/dataset_images.py
@@ -20,9 +20,9 @@ def get_tfrecord(phase_name, file_pattern, image_size,
         image = _augment(image, is_training)
         return (image, label)
     # On vérifie si phase_name est 'train' ou 'validation'
-    if phase_name not in ['train', 'eval']:
+    if phase_name not in ['train', 'valid']:
         raise ValueError('The phase_name %s is not recognized.\
-                          Please input either train or eval as the phase_name' % (phase_name))
+                          Please input either train or valid as the phase_name' % (phase_name))
     # TODO: Remove counting num_samples. num_samples have to be fixed before
     # Compte le nombre total d'examples dans tous les fichiers
     # file_pattern will have the following format:
@@ -51,9 +51,9 @@ def get_flat(phase_name, file_pattern, image_size,
         image = _augment(image, is_training)
         return image
     #On vérifie si phase_name est 'train' ou 'validation'
-    if phase_name not in ['train', 'eval']:
+    if phase_name not in ['train', 'valid']:
         raise ValueError('The phase_name %s is not recognized.\
-                          Please input either train or eval as the phase_name' % (phase_name))
+                          Please input either train or valid as the phase_name' % (phase_name))
     #TODO: Remove counting num_samples. num_samples have to be fixed before
     #Compte le nombre total d'examples dans tous les fichiers
     file_pattern_for_counting = file_pattern+ '_' + phase_name
@@ -85,7 +85,41 @@ def get_Mura(phase_name, file_pattern, image_size,
         image = _augment(image, is_training)
         return (image, label)
     #On vérifie si phase_name est 'train' ou 'validation'
-    if phase_name not in ['train', 'eval']:
+    if phase_name not in ['train', 'valid']:
+        raise ValueError('The phase_name %s is not recognized.\
+                          Please input either train or valid as the phase_name' % (phase_name))
+    #Using file_pattern, we replace the phase_name:
+    file_pattern_for_counting = file_pattern.replace("phase_name", phase_name)
+    #Use list file utiliy function, resulting in a tf.data.Dataset of filenames
+    dataset = tf.data.Dataset.list_files(file_pattern_for_counting)
+    #Introduce the parse_fn function in order to obtain the image and it's label for MURA dataset
+    dataset = dataset.map(_parse_fn, num_parallel_calls=os.cpu_count())
+    if is_training:
+        dataset = dataset.shuffle(buffer_size=shuffle_buffer_size)
+        dataset = dataset.repeat(num_epochs)    
+    dataset = dataset.batch(batch_size)
+    return dataset
+
+def get_GED(phase_name, file_pattern, image_size,
+            names_to_labels, num_classes, batch_size=32, num_epochs=-1,
+            shuffle_buffer_size=1024, is_training=False):
+    """Creates dataset based on phased_name(train or evaluation) for
+    rvl-cdip dataset
+    """
+    table = tf.contrib.lookup.index_table_from_tensor(mapping=tf.constant(names_to_labels))
+    def _parse_fn(filename):
+        #Create the keys_to_features dictionary for the decoder    
+        filename_split = tf.string_split([filename], delimiter=os.sep).values
+        #NOTE:The Following line is an efficient way of extracting label for MURA
+        #(ex: \data\MURA-v1.1\train\XR_ELBOW\patient00011\study1_negative\image.png)
+        label = tf.string_split([filename_split[-2]], delimiter = "_").values[-1]
+        label = tf.one_hot(table.lookup(label), num_classes)
+        image = tf.image.decode_png(tf.read_file(filename), channels=image_size[2])
+        image = tf.image.convert_image_dtype(image, dtype=tf.float32)
+        image = tf.image.resize_images(image, size=image_size[:2])
+        return (image, label)
+    #On vérifie si phase_name est 'train' ou 'validation'
+    if phase_name not in ['train', 'valid', 'test']:
         raise ValueError('The phase_name %s is not recognized.\
                           Please input either train or eval as the phase_name' % (phase_name))
     #Using file_pattern, we replace the phase_name:
@@ -99,12 +133,9 @@ def get_Mura(phase_name, file_pattern, image_size,
         dataset = dataset.repeat(num_epochs)    
     dataset = dataset.batch(batch_size)
     return dataset
-
-    
 def _augment(image, is_training=False):
     """
     Helper function for Data augmentation, depending on
     the case: train or validation set of data
     """
     return image
-
