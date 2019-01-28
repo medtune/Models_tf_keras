b580802ab68bfbacd35246d14adb98e569410ae0
Alaa El Bouchti <alaa_23@hotmail.fr>
2019-01-28 16:52:35 +0100

    fix bugs

--- models/cnn/finetune.py
+++ models/cnn/finetune.py
@@ -91,10 +91,10 @@ def assemble_gpus(model, classifier,
     """               
     assert optimizer_noun in optimizers.keys()
     # Using the ModelConstructor instance, we build our CNN architecture
-    with tf.device('/gpu:0'):
+    with tf.device_scope('/gpu:0'):
         features = model.construct()
     # Using the features previously extracted, we also build our classifier 
-    with tf.device('/gpu:1'):
+    with tf.device_scope('/gpu:1'):
         logits = classifier.construct(features)
     assembly = Model(inputs=model.input_placeholder, outputs=logits)
     # Using "get_loss" func, we retrieve the loss type (loss argument accepts a noun)
