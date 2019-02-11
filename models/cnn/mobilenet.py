6d4202978c4088277b84fe189fff2bb6cffd4483
Alaa El Bouchti <alaa_23@hotmail.fr>
2019-02-11 17:06:47 +0100

    prepare mobilenet architecture

---
+++ models/cnn/mobilenet.py
@@ -0,0 +1,13 @@
+"""
+The weights for all 16 models are obtained and translated
+from TensorFlow checkpoints found at :
+    (https://github.com/tensorflow/models/blob/master/research/slim/nets/mobilenet_v1.md)
+# Reference
+- [MobileNets: Efficient Convolutional Neural Networks for
+   Mobile Vision Applications](https://arxiv.org/pdf/1704.04861.pdf))
+# Keras Implementation:
+    (https://github.com/keras-team/keras-applications/blob/master/keras_applications/mobilenet.py)
+"""
+
+
+import tensorflow.keras as keras
