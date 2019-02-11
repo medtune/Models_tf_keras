312210616b8582802847713da09a6ff7ec14a849
Alaa El Bouchti <alaa_23@hotmail.fr>
2019-02-11 16:35:27 +0100

    Adding refrences for Resnet's script dev

---
+++ models/cnn/resnet50.py
@@ -0,0 +1,20 @@
+"""ResNet, ResNetV2, and ResNeXt models for Keras.
+# Reference papers
+- [Deep Residual Learning for Image Recognition]
+    (https://arxiv.org/abs/1512.03385) (CVPR 2016 Best Paper Award)
+- [Identity Mappings in Deep Residual Networks]
+    (https://arxiv.org/abs/1603.05027) (ECCV 2016)
+- [Aggregated Residual Transformations for Deep Neural Networks]
+    (https://arxiv.org/abs/1611.05431) (CVPR 2017)
+# Reference implementations
+- [TensorNets]
+    (https://github.com/taehoonlee/tensornets/blob/master/tensornets/resnets.py)
+- [Caffe ResNet]
+    (https://github.com/KaimingHe/deep-residual-networks/tree/master/prototxt)
+- [Torch ResNetV2]
+    (https://github.com/facebook/fb.resnet.torch/blob/master/models/preresnet.lua)
+- [Torch ResNeXt]
+    (https://github.com/facebookresearch/ResNeXt/blob/master/models/resnext.lua)
+-  [keras_applications]
+    https://github.com/keras-team/keras-applications/blob/master/keras_applications/resnet_common.py
+"""
