9d2a258a388801c22995e5de9aec21b38f078f01
Alaa El Bouchti <alaa_23@hotmail.fr>
2019-03-11 15:52:39 +0100

    Correct 'bn_axis' to 'axis' in densenet batchnorm arguments

--- models/cnn/famous_cnn/densenet.py
+++ models/cnn/famous_cnn/densenet.py
@@ -133,7 +133,7 @@ def densenet(blocks,
                                         name=naming+"conv1_bn")(x)
     x = keras.layers.Activation(activation, name=naming+"conv1"+activation)(x)
     x = keras.layers.ZeroPadding2D(((1,1),(1,1)))(x)
-    x = keras.layers.MaxPool(3, strides=2, name=naming+'pool1')(x)
+    x = keras.layers.MaxPool2D(3, strides=2, name=naming+'pool1')(x)
     x = _dense_block(x, blocks[0], naming+'conv2')
     x = _transition_block(x, 0.5, naming+'pool2')
     x = _dense_block(x, blocks[1], naming+'conv3')
